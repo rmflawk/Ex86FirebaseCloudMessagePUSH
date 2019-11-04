d9d84abef9d70c725ab0156ccb3acbe5e6309f9a
rmflawk <ant921111@naver.com>
2019-11-04 17:27:39 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/youngstudio/ex86firebasecloudmessagepush/MessageActivity.java
@@ -0,0 +1,36 @@
+package com.youngstudio.ex86firebasecloudmessagepush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    String s;
+
+    public MessageActivity(String s) {
+        this.s = s;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+
+        tv= findViewById(R.id.tv);
+
+        Intent intent= getIntent();
+
+        String name= intent.getStringExtra("name");
+        String msg= intent.getStringExtra("msg");
+
+        tv.setText(name +"\n" + msg + "\n");
+
+    }
+
+}
